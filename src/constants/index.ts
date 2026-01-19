e7e7343eca550f794e220dce9b7ef265f34fc0d0
Bryson Walter <Bryson_Work@hotmail.com>
2026-01-19 22:52:05 +1100

    Fix empty constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export const APP_NAME = 'CentreGuide';
+export const APP_VERSION = '1.0.0';
+
+export const STORAGE_KEYS = {
+  AUTH_TOKEN: 'auth_token',
+  USER_DATA: 'user_data',
+  PREFERENCES: 'preferences',
+} as const;
